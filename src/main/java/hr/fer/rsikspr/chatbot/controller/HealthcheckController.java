3259096d4e27342a449b79fe1d533035568f5580
Danko Čurlin <55360422+Theanko1412@users.noreply.github.com>
2023-06-25 05:27:03 +0200

    Feat: nginx setup (#8)

--- src/main/java/hr/fer/rsikspr/chatbot/controller/HealthcheckController.java
+++ src/main/java/hr/fer/rsikspr/chatbot/controller/HealthcheckController.java
@@ -1,6 +1,7 @@
 package hr.fer.rsikspr.chatbot.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Healthcheck")
 public class HealthcheckController {
 
+  private final String instanceId;
+
+  // each application instance will have a unique id
+  public HealthcheckController() {
+    this.instanceId = UUID.randomUUID().toString();
+  }
+
   @GetMapping(value = "/health")
   public String performHealthcheck() {
-    return "OK";
+    return "Instance ID: " + instanceId + " is OK";
   }
 }
