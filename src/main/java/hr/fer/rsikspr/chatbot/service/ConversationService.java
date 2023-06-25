ba5a12ceff0614910546561930a09e05506e03f3
Danko Čurlin <55360422+Theanko1412@users.noreply.github.com>
2023-06-25 16:04:16 +0200

    Feat: search convos by date (#10)

--- src/main/java/hr/fer/rsikspr/chatbot/service/ConversationService.java
+++ src/main/java/hr/fer/rsikspr/chatbot/service/ConversationService.java
@@ -2,12 +2,16 @@ package hr.fer.rsikspr.chatbot.service;
 
 import hr.fer.rsikspr.chatbot.model.entity.Conversation;
 import hr.fer.rsikspr.chatbot.model.entity.Message;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ConversationService {
 
   Conversation getConversationById(String conversationId);
 
   Conversation getConversationByParticipants(String sender, String receiver);
+
+  List<Conversation> getConversationsBetweenDates(LocalDateTime startDate, LocalDateTime endDate);
 
   Conversation closeConversation(String conversationId);
 
